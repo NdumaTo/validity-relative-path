cf352ce1b82e23fda2fa3b4a350a8d4f07fda3ef
Tola Ndumari <tola.ndumari@clock.co.uk>
2020-01-24 09:24:53 +0000

    Adds validation for url

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('test')
+const validity = require('validity'),
+  isValid = require('./lib/relative-path-validator')
+
+module.exports = validity.createValidatorAllowingFailureMessageOverride(isValid)
